c6e1aeda2d5019ca793937ebd3c3326956c0e35e
gilbertoca <gilbertoca@gmail.com>
2014-11-26 11:17:22 -0300

    correction by Ceyda see comment on http://stackoverflow.com/a/24360708/269514

--- src/main/java/sandbox/primefaces/datatable/fix/LazySelectionFeature.java
+++ src/main/java/sandbox/primefaces/datatable/fix/LazySelectionFeature.java
@@ -2,6 +2,7 @@ package sandbox.primefaces.datatable.fix;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import javax.faces.FacesException;
@@ -14,85 +15,86 @@ import org.primefaces.util.ComponentUtils;
  *
  * @author Ceyda
  */
-public class LazySelectionFeature extends org.primefaces.component.datatable.feature.SelectionFeature {
+public class LazySelectionFeature extends org.primefaces.component.datatable.feature.SelectionFeature{
 
     @Override
     public void decode(FacesContext context, DataTable table) {
         String clientId = table.getClientId(context);
-        Map<String, String> params = context.getExternalContext().getRequestParameterMap();
+        Map<String,String> params = context.getExternalContext().getRequestParameterMap();
 
         String selection = params.get(clientId + "_selection");
 
-        if (table.isSingleSelectionMode()) {
+        if(table.isSingleSelectionMode())
             decodeSingleSelection(table, selection);
-        } else {
+        else
             decodeMultipleSelection(context, table, selection);
-        }
     }
 
     void decodeSingleSelection(DataTable table, String selection) {
-        if (ComponentUtils.isValueBlank(selection)) {
-            table.setSelection(null);
-        } else {
-            table.setSelection(table.getRowData(selection));
+            if(ComponentUtils.isValueBlank(selection))
+                table.setSelection(null);
+            else
+                table.setSelection(table.getRowData(selection));
         }
-    }
 
     void decodeMultipleSelection(FacesContext context, DataTable table, String selection) {
         Class<?> clazz = table.getValueExpression("selection").getType(context.getELContext());
         boolean isArray = clazz.isArray();
 
-        if (!isArray && !List.class.isAssignableFrom(clazz)) {
+        if(!isArray && !List.class.isAssignableFrom(clazz)) {
             throw new FacesException("Multiple selection reference must be an Array or a List for datatable " + table.getClientId());
         }
 
-        if (!isArray && !List.class.isAssignableFrom(clazz)) {
-            throw new FacesException("Multiple selection reference must be an Array or a List for datatable " + table.getClientId());
-        }
-
-        if (ComponentUtils.isValueBlank(selection)) {
-            if (isArray) {
+        if(ComponentUtils.isValueBlank(selection)) {
+            if(isArray) {
                 table.setSelection(Array.newInstance(clazz.getComponentType(), 0));
-            } else {
+            }
+            else {
                 table.setSelection(new ArrayList<Object>());
             }
-        } else {
+        }
+        else {
             String[] rowKeys = selection.split(",");
             List<Object> selectionList = new ArrayList<Object>();
 
-            boolean lazy = table.isLazy();
+            boolean lazy=table.isLazy();
             if (lazy) {
-                if (table.getSelection() != null) {
-                    selectionList.addAll((List<Object>) table.getSelection());
+
+            List<String> currentRowKeys = new ArrayList<String>(Arrays.asList(rowKeys));
+            if (table.getSelection() != null) {
+                List<Object> alreadySelected = (List<Object>) table.getSelection();
+
+                for (Object object : alreadySelected) {//For deselecting
+                    Object rowKeyFromModel = table.getRowKeyFromModel(object);
+                    if (currentRowKeys.contains(rowKeyFromModel)) {
+                        selectionList.add(object);
+                        currentRowKeys.remove(rowKeyFromModel);
+                    } 
+                }                    
+            }
+            for (String key : currentRowKeys) {//For selecting  
+                Object rowData = table.getRowData(key);
+                if (rowData != null && !selectionList.contains(rowData)) {
+                       selectionList.add(rowData);
                 }
-                for (int i = 0; i < rowKeys.length; i++) {
+            }
+
+        }else{
+
+                for(int i = 0; i < rowKeys.length; i++) {
                     Object rowData = table.getRowData(rowKeys[i]);
 
-                    if (rowData != null) {
-                        if (!selectionList.contains(rowData)) {
-                            selectionList.add(rowData);
-                        } else {
-                            selectionList.remove(rowData);
-                        }
-                    }
-                }
-
-            } else {
-
-                for (int i = 0; i < rowKeys.length; i++) {
-                    Object rowData = table.getRowData(rowKeys[i]);
-
-                    if (rowData != null) {
+                    if(rowData != null)
                         selectionList.add(rowData);
-                    }
                 }
 
             }
 
-            if (isArray) {
+            if(isArray) {
                 Object selectionArray = Array.newInstance(clazz.getComponentType(), selectionList.size());
                 table.setSelection(selectionList.toArray((Object[]) selectionArray));
-            } else {
+            }
+            else {
                 table.setSelection(selectionList);
             }
         }
